29618dfb8e88557fb019944e8714bba504512766
lemelinjs <lemelin.jean-sebastien@carrefour.cegepvicto.ca>
2018-04-03 23:20:42 -0400

    premières sauvegardes avec Mongoose

--- API/models/groupe.js
+++ API/models/groupe.js
@@ -3,23 +3,13 @@ var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
 
 var schema = new Schema({
-    nom: {type: String, required: true},
-    actif: {type: boolean},
-    public: {type: boolean},
-    commenter: {type: boolean},
-    proprietaire: {type: Schema.Types.ObjectId, ref: 'utilisateur', unique: true},
-    super_admin: [{type: Schema.Types.ObjectId, ref: 'utilisateur'}],
-    admin: [{type: Schema.Types.ObjectId, ref: 'utilisateur'}],
-    membres: [{type: Schema.Types.ObjectId, ref: 'utilisateur'}],
-    classes: [{type: Schema.Types.ObjectId, ref: 'classe'}],
-    programmes: [{type: Schema.Types.ObjectId, ref: 'programme'}],
-    types:[{type: Schema.Types.ObjectId, ref: 'programme'}],//Étudiant, prof, staff
-    banis:[{type: Schema.Types.ObjectId, ref: 'utilisateur'}],
-    date_fin: {type: Date},
-    code_inscription: {type: String, unique:true}
+    nom: {type: String},
+    actif: {type: Boolean},
+    public: {type: Boolean, required: false},
+    utilisateurs:[{type:Schema.Types.ObjectId, ref: 'utilisateur', required: false}]
 });
 
 //PLugin pour valider un champs unique
-Schema.plugins(mongooseUniqueValidator);
+schema.plugin(mongooseUniqueValidator);
 
 module.exports = mongoose.model('groupe', schema);
--- API/routes/API/groupes/groupes.js
+++ API/routes/API/groupes/groupes.js
@@ -4,21 +4,14 @@
  */
 var express = require('express');
 var router = express.Router();
+var Groupe = require('../../../models/groupe');
 
-router.get('/', function(req, res, next) {
-  MongoClient.connect(url, function(err, client) {
-      assert.equal(null, err);
-      console.log("Connexion au serveur réussie");
-      const db = client.db(dbName);
-
-      db.collection('produits').find().toArray(function(err, result) {
-          if (err) return console.log(err)
-          console.log(result);
-          res.json(result)
-      })
-
-      client.close();
-  });
+router.post('/', function(req, res, next) {
+    var groupe = new Groupe({
+        nom:'Apprendrsdfasdfe',
+        actif:1
+    });
+    groupe.save();
 });
 
 
