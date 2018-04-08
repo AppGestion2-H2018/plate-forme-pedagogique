5e57bd5d2d94dfc9c7bbe9e14e3f71639a75dce7
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2018-04-08 16:45:15 -0400

    correction de la route des publications

--- API/routes/API/publications/publications.js
+++ API/routes/API/publications/publications.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('api Groupes');
+  res.send('api Publications');
 });
 
 module.exports = router;
