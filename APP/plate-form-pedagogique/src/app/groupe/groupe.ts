b416ccc3a70c661d2ae3332e3639f2a1736da532
DannyDugas1992 <dugas.danny@carrefour.cegepvicto.ca>
2018-04-12 16:57:39 -0400

    Creation du fichier Groupe.ts

---
+++ APP/plate-form-pedagogique/src/app/groupe/groupe.ts
@@ -0,0 +1,16 @@
+export class Groupe {
+  _id: number;
+  nom: string;
+  date_fin: Date;
+  actif: Boolean;
+  est_publique: Boolean;
+  commenter: Boolean;
+  super_admin: number;
+  admin: number;
+  programmes: number;
+  classes: number;
+  type: number;
+  utilisateur: number;
+  blacklist: number;
+  
+}
