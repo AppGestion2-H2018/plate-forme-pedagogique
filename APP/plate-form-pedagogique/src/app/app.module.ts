6a2e2d89b3f39f130cba1dce248bd8f5a744e323
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2018-04-12 16:36:34 -0400

    Ajout de Material Design

--- APP/plate-form-pedagogique/src/app/app.module.ts
+++ APP/plate-form-pedagogique/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatSidenavModule, MatToolbarModule, MatCardModule, MatGridListModule, MatListModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BiblioComponent } from './biblio/biblio.component';
@@ -8,6 +10,12 @@ import { ConnecterUtilisateurComponent } from './utilisateurs/connecter-utilisat
 import { CreerUtilisateurComponent } from './utilisateurs/creer-utilisateur/creer-utilisateur.component';
 import { ModifierUtilisateurComponent } from './utilisateurs/modifier-utilisateur/modifier-utilisateur.component';
 import { RecupererMotDePasseComponent } from './utilisateurs/recuperer-mot-de-passe/recuperer-mot-de-passe.component';
+import { PrincipalUtilisateursComponent } from './utilisateurs/principal-utilisateurs/principal-utilisateurs.component';
+import { MenuUtilisateursComponent } from './utilisateurs/menu-utilisateurs/menu-utilisateurs.component';
+import { PrincipalBiblioComponent } from './biblio/principal-biblio/principal-biblio.component';
+import { MenuBiblioComponent } from './biblio/menu-biblio/menu-biblio.component';
+import { MenuPublicationsComponent } from './publications/menu-publications/menu-publications.component';
+import { PrincipalPublicationsComponent } from './publications/principal-publications/principal-publications.component';
 
 
 @NgModule({
@@ -17,10 +25,23 @@ import { RecupererMotDePasseComponent } from './utilisateurs/recuperer-mot-de-pa
     ConnecterUtilisateurComponent,
     CreerUtilisateurComponent,
     ModifierUtilisateurComponent,
-    RecupererMotDePasseComponent
+    RecupererMotDePasseComponent,
+    PrincipalUtilisateursComponent,
+    MenuUtilisateursComponent,
+    PrincipalBiblioComponent,
+    MenuBiblioComponent,
+    MenuPublicationsComponent,
+    PrincipalPublicationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
