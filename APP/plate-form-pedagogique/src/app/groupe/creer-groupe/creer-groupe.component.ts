aa0dd1e932a7455d40eea75fc452abd0db6cab4d
unknown <chartier.andy@carrefour.cegepvicto.ca>
2018-04-17 15:45:15 -0400

    Correction d'erreur du servive des groupes

--- APP/plate-form-pedagogique/src/app/groupe/creer-groupe/creer-groupe.component.ts
+++ APP/plate-form-pedagogique/src/app/groupe/creer-groupe/creer-groupe.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupeService } from '../service/groupe.service';
+
 @Component({
   selector: 'app-creer-groupe',
   templateUrl: './creer-groupe.component.html',
   styleUrls: ['./creer-groupe.component.css'],
-  providers:[TaskService]
+  providers:[GroupeService]
 })
 export class CreerGroupeComponent implements OnInit {
 
-  constructor(private groupeService:GroupService) { }
+  constructor(private groupeService:GroupeService) { }
 
   ngOnInit() {
   }
