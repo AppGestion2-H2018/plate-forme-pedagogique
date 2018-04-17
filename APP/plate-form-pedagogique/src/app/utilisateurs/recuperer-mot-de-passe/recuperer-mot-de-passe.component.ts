6d71dbf15dfed456c4ed30e956cf6c1c451807c3
FrechetteMathieu <frechette.mathieu@carrefour.cegepvicto.ca>
2018-04-17 13:48:54 -0400

    Creation de la classe et du service utilisateur

--- APP/plate-form-pedagogique/src/app/utilisateurs/recuperer-mot-de-passe/recuperer-mot-de-passe.component.ts
+++ APP/plate-form-pedagogique/src/app/utilisateurs/recuperer-mot-de-passe/recuperer-mot-de-passe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/utilisateur.service';
 
 @Component({
   selector: 'app-recuperer-mot-de-passe',
@@ -9,15 +10,15 @@ export class RecupererMotDePasseComponent implements OnInit {
 
   courriel: string;
 
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   // Envoi du courriel de réinitialisation du mot de passe
-  sendMail():void{
+  forgotpassword():void{
     console.log(this.courriel);
     // Valide si le courriel est présent dans la bd
 
     // Appelle la fonction d'envoi de courriel du server
-    
+
   }
 
   ngOnInit() {
