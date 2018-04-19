42fb1abc54f9639582cefe6ec501fabbe82adaa6
Francis Gagnon <gagnon.francis@carrefour.cegepvicto.ca>
2018-04-19 16:10:55 -0400

    Ajout du module MySQL et des squelettes des components

---
+++ APP/plate-form-pedagogique/src/app/resultats/afficher-resultats/afficher-resultats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-afficher-resultats',
+  templateUrl: './afficher-resultats.component.html',
+  styleUrls: ['./afficher-resultats.component.css']
+})
+export class AfficherResultatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
