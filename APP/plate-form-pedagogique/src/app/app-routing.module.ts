0aa5b99b8bf47f6e568814a8c636af3fb841676e
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2018-04-23 13:48:19 -0400

    intégration des routes

--- APP/plate-form-pedagogique/src/app/app-routing.module.ts
+++ APP/plate-form-pedagogique/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import {PrincipalPublicationsComponent} from './publications/principal-publications/principal-publications.component';
+import {PrincipalEvenementsComponent} from './evenement/principal-evenements/principal-evenements.component';
+import {PrincipalBiblioComponent} from './biblio/principal-biblio/principal-biblio.component';
+import {PrincipalResultatsComponent} from './resultats/principal-resultats/principal-resultats.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'publications', component: PrincipalPublicationsComponent },
+    { path: 'evenements', component: PrincipalEvenementsComponent },
+    { path: 'biblio', component: PrincipalBiblioComponent },
+    /*{ path: 'groupes', component: PrincipalGroupesComponent }*/
+    { path: 'resultats', component: PrincipalResultatsComponent },
+    { path: 'home', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    exports: [ RouterModule ],
+    imports: [ RouterModule.forRoot(routes) ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
