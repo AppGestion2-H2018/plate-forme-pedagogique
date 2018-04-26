034ce3a2c3bd6783aa9a81c905e84fa959c723bc
Philip05 <drouin.philip@carrefour.cegepvicto.ca>
2018-04-26 15:32:41 -0400

    Sélection, suppression et ajout d'événements à partir du calendrier fonctionnel

--- APP/plate-form-pedagogique/src/app/evenement/models/event.ts
+++ APP/plate-form-pedagogique/src/app/evenement/models/event.ts
@@ -1,35 +1,12 @@
-// import {JsonObject, JsonProperty} from "json2typescript";
-//
 export class Event {
-
-    id: string;
+    _id: string;
     text: string;
-    // @JsonProperty('description')
     description: string;
     categorie: string;
-    // @JsonProperty('date_debut')
     start_date: string;
-    // @JsonProperty('date_fin')
     end_date: string;
     type: string;
     admin_id:string;
     group_id: string;
     active: boolean;
 }
-
-//     @JsonProperty('description')
-//     getdescription() : string {
-//         return "text";
-//     }
-//
-//     @JsonProperty('date_debut')
-//     getdate_debut(){
-//         return "start_date";
-//     }
-//
-//     @JsonProperty('date_fin')
-//     getdate_fin(){
-//         return "end_date";
-//     }
-// }
-//
