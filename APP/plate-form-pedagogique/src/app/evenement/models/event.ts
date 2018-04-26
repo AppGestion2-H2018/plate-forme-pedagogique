c0b9df137c452feb764c424863c5ee7e00f82c1a
Philip05 <drouin.philip@carrefour.cegepvicto.ca>
2018-04-26 15:39:21 -0400

    Réparation du modèle

--- APP/plate-form-pedagogique/src/app/evenement/models/event.ts
+++ APP/plate-form-pedagogique/src/app/evenement/models/event.ts
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-export class Event {
-    _id: string;
-    text: string;
-    description: string;
-    categorie: string;
-=======
-// import {JsonObject, JsonProperty} from "json2typescript";
-// Définition d’une classe Evenement
 export class Event {
     id: string;
-    // @JsonProperty('date_debut')
->>>>>>> 51e98a862408bd00f4da8c87fd0e4525566a49bc
     start_date: string;
     end_date: string;
     text: string;
     active: boolean;
-    // @JsonProperty('description')
     description: string;
     categorie: string;
     type: string;
