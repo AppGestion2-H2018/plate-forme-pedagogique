0297069d36a10c089b36205d8db965c937f8a631
JessicaLabrieLamontagne <labrie-lamontagne.jessica@cgpvicto.info>
2018-04-30 16:19:57 -0500

    modification event.service

--- APP/plate-form-pedagogique/src/app/evenement/evenement.service.ts
+++ APP/plate-form-pedagogique/src/app/evenement/evenement.service.ts
@@ -10,21 +10,21 @@ const httpOptions = {
 };
 @Injectable()
 export class EvenementService {
-    // private eventsAPIUrl = 'https://api-appgestion2-h18.herokuapp.com/api/evenements';
-    //
-    //
-    // constructor(private http: HttpClient) {console.log('Event Service Initialized...');
-    // }
-    //
-    //
-    // getEvents(): Observable<Event[]> {
-    //     return this.http.get<Event[]>(this.eventsAPIUrl + '/events');
-    // }
-    //
-    //
-    // updateStatus(event : Event): Observable<any> {
-    //     const id = typeof event === 'number' ? event : event.id;
-    //     const url = `${this.eventsAPIUrl}/event/${id}`;   // ajouter l'id à l'URL de base
-    //     return this.http.put<Event>(url, event, httpOptions);
-    // }
+    private eventsAPIUrl = 'https://api-appgestion2-h18.herokuapp.com/api/evenements';
+
+
+    constructor(private http: HttpClient) {console.log('Event Service Initialized...');
+    }
+
+
+    getEvents(): Observable<Event[]> {
+        return this.http.get<Event[]>(this.eventsAPIUrl + '/events');
+    }
+
+
+    updateStatus(event : Event): Observable<any> {
+        const id = typeof event === 'number' ? event : event.id;
+        const url = `${this.eventsAPIUrl}/event/${id}`;   // ajouter l'id à l'URL de base
+        return this.http.put<Event>(url, event, httpOptions);
+    }
 }
