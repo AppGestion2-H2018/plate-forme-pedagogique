519896f6789157a9596f4d34dae99dc2d3ce90aa
williamHoule <houle.william@carrefour.cegepvicto.ca>
2018-05-03 18:32:20 -0400

    résolution des problèmes dans les services

--- APP/plate-form-pedagogique/src/app/resultats/resultat.service.ts
+++ APP/plate-form-pedagogique/src/app/resultats/resultat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Resultat } from './resultat';
+import { Evaluation } from './evaluation';
+import { Note } from './note';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
@@ -13,8 +14,8 @@ export class ResultatService {
       return this.http.get<Evaluation[]>('https://api-appgestion2-h18.herokuapp.com/api/resultats/evaluations');
   }
   
-  getNotes(): Observable<Notes[]> {
-      return this.http.get<Notes[]>('https://api-appgestion2-h18.herokuapp.com/api/resultats/notes');
+  getNotes(): Observable<Note[]> {
+      return this.http.get<Note[]>('https://api-appgestion2-h18.herokuapp.com/api/resultats/notes');
   }
 
 }
