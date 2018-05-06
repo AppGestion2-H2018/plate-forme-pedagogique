7817509999e38c806b56b635f0763b8abb619734
MasseLM <louis-mathieu.masse@cegepvicto.info>
2018-05-06 16:22:08 -0400

    Ajout-Evenement avec verifications
    Ajouts des vérifications dans le formulaire et dans le component.ts

--- APP/plate-form-pedagogique/src/app/evenement/ajout-evenement/ajout-evenement.component.ts
+++ APP/plate-form-pedagogique/src/app/evenement/ajout-evenement/ajout-evenement.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+import {Event} from '../models/event';
+import { EvenementService } from '../../service/evenement.service'
+
 
 @Component({
-  selector: 'app-ajout-evenement',
-  templateUrl: './ajout-evenement.component.html',
-  styleUrls: ['./ajout-evenement.component.css']
+    selector: 'app-ajout-evenement',
+    templateUrl: './ajout-evenement.component.html',
+    styleUrls: ['./ajout-evenement.component.css']
 })
 export class AjoutEvenementComponent implements OnInit {
 
-  constructor() { }
+    evenementChoisi: Event;
 
-  ngOnInit() {
-  }
+    constructor(private EvenementService: EvenementService) { }
+
+    addEvenement(evenement: Event, evenementFormAjout: NgForm) {
+
+        if(evenementFormAjout.valid) {
+            this.EvenementService.addEvenement(this.evenementChoisi)
+                .subscribe(
+                )
+        }
+        else{
+
+            console.log("problème avec la validation ?");
+        }
+    }
+
+    ngOnInit() {
+
+        this.evenementChoisi = new Event();
+    }
 
 }
--- APP/plate-form-pedagogique/src/app/service/evenement.service.ts
+++ APP/plate-form-pedagogique/src/app/service/evenement.service.ts
@@ -1,22 +1,24 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Rx';
-import {Evenement} from '../class/evenement';
+import {Event} from '../evenement/models/event';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class EvenementService {
    constructor(private http: HttpClient ) {
        console.log('Service d\'événements Initialisé...');
    }
-    getEvenements(): Observable<Evenement[]> {
-        return this.http.get<Evenement[]>('http://localhost:3000/API/evenements');
+    getEvenements(): Observable<Event[]> {
+        return this.http.get<Event[]>('http://localhost:3000/API/evenements');
     }
-    /*addEvenement(newEvenement: Evenement) {
-        var headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/API/evenements', JSON.stringify(newEvenement), {headers : headers});
-    }*/
+    addEvenement(newEvenement: Event) {
+        return this.http.post('https://api-appgestion2-h18.herokuapp.com/api/evenements/ajout', JSON.stringify(newEvenement), httpOptions);
+    }
     detruireEvenement(id: string) {
         return this.http.delete('http://localhost:3000/API/evenements/' + id);
     }
