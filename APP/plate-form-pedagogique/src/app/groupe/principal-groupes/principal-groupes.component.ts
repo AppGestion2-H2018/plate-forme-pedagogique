5a2bd153618680b34aec10b9b38f8aa6e71dead5
Jean-Sébastien <jslemelin@entretienscem.com>
2018-05-14 14:24:31 -0400

    Ajouter l'utilisateur dans le app group principale pour accéder à toutes ses infos

--- APP/plate-form-pedagogique/src/app/groupe/principal-groupes/principal-groupes.component.ts
+++ APP/plate-form-pedagogique/src/app/groupe/principal-groupes/principal-groupes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {CookieService} from "ngx-cookie-service";
+import {Utilisateur} from "../../class/utilisateur";
+import {UtilisateurService} from "../../service/utilisateur.service";
 
 @Component({
   selector: 'app-principal-groupes',
@@ -9,13 +11,31 @@ import {CookieService} from "ngx-cookie-service";
 export class PrincipalGroupesComponent implements OnInit {
 
   userId: string;
+  utilisateur = new Utilisateur();
 
-  constructor(private cookiService : CookieService) {
+  constructor(private cookiService : CookieService, private utilisateurService: UtilisateurService) {
       this.userId = this.cookiService.get("auth_id");
+      this.utilisateur._id = this.userId;
+  }
+
+  getUtilisateur() : void
+  {
+      this.utilisateur._id = this.userId;
+      this.utilisateurService.getUtilisateur(this.utilisateur).subscribe(
+          data => {
+              this.utilisateur = data;
+          },
+          error => {
+              console.error(error);
+          }
+      );
   }
 
   ngOnInit() {
-
+    this.getUtilisateur();
+    console.log(this.utilisateur);
   }
+
+
 
 }
