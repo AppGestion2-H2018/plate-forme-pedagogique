55d719bc863b4ca1dfe21f49950e1ba11af6bd65
MiguelOuellette <ouellette.miguel@carrefour.cegepvicto.ca>
2018-05-15 11:52:00 -0400

    Modification Publication
    N'importe qui peut pour le moment modifier n'importe quel publication
    via un bouton

--- APP/plate-form-pedagogique/src/app/app-routing.module.ts
+++ APP/plate-form-pedagogique/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import {PrincipalGroupesComponent} from './groupe/principal-groupes/principal-gr
 import {RecupererMotDePasseComponent} from './utilisateurs/recuperer-mot-de-passe/recuperer-mot-de-passe.component';
 import {CreerUtilisateurComponent} from './utilisateurs/creer-utilisateur/creer-utilisateur.component';
 import {AjoutPublicationComponent} from './publications/ajout-publication/ajout-publication.component';
+import {ModificationPublicationComponent} from './publications/modification-publication/modification-publication.component';
 import {AjoutEvenementComponent} from './evenement/ajout-evenement/ajout-evenement.component';
 import {ModifierMotDePasseComponent} from './utilisateurs/modifier-mot-de-passe/modifier-mot-de-passe.component';
 import {ClassesComponent} from "./groupe/classes/classes.component";
@@ -37,6 +38,7 @@ const routes: Routes = [
     { path: 'utilisateurs/creer', component: CreerUtilisateurComponent },
     { path: 'utilisateurs/:utilisateurId/modifier', component: CreerUtilisateurComponent }, // Exemple: http://localhost:4200/utilisateurs/5aeb6574db8aa807b08c8602/modifier
     { path: 'publications/ajout', component: AjoutPublicationComponent},
+    { path: 'publications/:id', component: ModificationPublicationComponent},
     { path: 'ajout-evenement', component: AjoutEvenementComponent },
     { path: 'motdepasse-reinitialisation/:resetPasswordToken', component: ModifierMotDePasseComponent },
     { path: 'connexion', component: ConnecterUtilisateurComponent },
