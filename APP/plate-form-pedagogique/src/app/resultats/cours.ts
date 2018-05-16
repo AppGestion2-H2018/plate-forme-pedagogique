7d991076dc0318e7306e9608f8b81635410c7486
Francis Gagnon <gagnon.francis@carrefour.cegepvicto.ca>
2018-05-15 20:17:37 -0400

    Upgrade des résultats scolaires (prise 2)
    Ajout et amélioration des classes nécessaires aux résultats scolaires
    Ajout de plusieurs routes dans l'API
    Ajout de plusieurs functions dans le service des résultats
    Amélioration et réparation de la connection MySQL à la BD

---
+++ APP/plate-form-pedagogique/src/app/resultats/cours.ts
@@ -0,0 +1,6 @@
+// Définition d’une classe Cours
+export class Cours {
+    id: number;
+    nom : string;
+    session : string;
+  }
---
+++ APP/plate-form-pedagogique/src/app/resultats/grilleevaluation.ts
@@ -0,0 +1,7 @@
+// Définition d’une classe Grilleevaluation
+export class Grilleevaluation {
+    id: number;
+    enseignant_id : number;
+	cours_id: number;
+    groupe_id: number;
+  }
