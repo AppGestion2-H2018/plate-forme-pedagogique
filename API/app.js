cbd70a8aeba952c63a72ed2d79dba6c8cd607ad1
Francis Gagnon <gagnon.francis@carrefour.cegepvicto.ca>
2018-05-15 20:26:07 -0400

    Revert "Merge branch 'master' of https://github.com/AppGestion2-H2018/plate-forme-pedagogique"
    This reverts commit 05c8df6d78bd42b58173c1e2c9f57564782c30b4, reversing
    changes made to 559732abe3bbd18731f0cf2458aaba28ac71bcdb.

--- API/app.js
+++ API/app.js
@@ -68,7 +68,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/utilisateurs', utlisateursRouter);
 
 
@@ -97,6 +96,7 @@ app.use(function (req, res, next) {
     }
 });
 
+app.use('/', indexRouter);
 app.use('/api/bibliotheque', bibliothequeRouter);
 app.use('/api/evenements', evenementsRouter);
 app.use('/api/groupes', groupesRouter);
